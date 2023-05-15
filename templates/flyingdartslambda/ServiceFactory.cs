39883c0a567c333fa5b57bdb1e85b163f8404e2c
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-15 22:51:51 +0200

    Fix workflow, serviceFactory and add project to solution

--- templates/flyingdartslambda/ServiceFactory.cs
+++ templates/flyingdartslambda/ServiceFactory.cs
@@ -8,7 +8,15 @@ public static class ServiceFactory
 {
     public static ServiceProvider GetServiceProvider()
     {
+        var configuration = new ConfigurationBuilder()
+            .AddSystemsManager($"/{System.Environment.GetEnvironmentVariable("EnvironmentName")}/Application")
+            .Build();
+        
         var services = new ServiceCollection();
+        services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+        services.AddAWSService<IAmazonDynamoDB>(configuration.GetAWSOptions("DynamoDb"));
+        services.AddOptions<ApplicationOptions>();
+        services.AddTransient<IDynamoDBContext, DynamoDBContext>();
         services.AddValidatorsFromAssemblyContaining<$command$Validator>();
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof($command$).Assembly));
         return services.BuildServiceProvider();
