d797b40607e6c9fba5636a20c6f468b4290be100
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-16 10:20:50 +0200

    Added documentation

--- templates/flyingdartslambda/Function.cs
+++ templates/flyingdartslambda/Function.cs
@@ -3,17 +3,26 @@ using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using Flyingdarts.Lambdas.Shared;
 
+// Get the service provider
 var services = ServiceFactory.GetServiceProvider();
+
+// Create an instance of the InnerHandler using the service provider
 var innerHandler = new InnerHandler(services);
+
+// Create a serializer for JSON serialization and deserialization
 var serializer = new DefaultLambdaJsonSerializer(x => x.PropertyNameCaseInsensitive = true);
 
-// The function handler that will be called for each Lambda event
+// Define the Lambda function handler
 var handler = async (APIGatewayProxyRequest request) =>
 {
+    // Convert the APIGatewayProxyRequest to the specified $command$ type using the serializer
     var socketRequest = request.To<$command$>(serializer);
+
+    // Handle the socketRequest using the innerHandler
     return await innerHandler.Handle(socketRequest);
 };
 
+// Create and run the Lambda function
 await LambdaBootstrapBuilder.Create(handler, serializer)
     .Build()
     .RunAsync();
--- templates/flyingdartslambda/ServiceFactory.cs
+++ templates/flyingdartslambda/ServiceFactory.cs
@@ -1,4 +1,3 @@
-
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,21 +5,42 @@ using FluentValidation;
 using Flyingdarts.Shared;
 using Microsoft.Extensions.Configuration;
 
+/// <summary>
+/// Factory class for creating the service provider.
+/// </summary>
 public static class ServiceFactory
 {
+    /// <summary>
+    /// Creates and configures the service provider.
+    /// </summary>
+    /// <returns>The configured service provider.</returns>
     public static ServiceProvider GetServiceProvider()
     {
+        // Build the configuration using AWS Systems Manager Parameter Store.
         var configuration = new ConfigurationBuilder()
             .AddSystemsManager($"/{System.Environment.GetEnvironmentVariable("EnvironmentName")}/Application")
             .Build();
-        
+
+        // Create a new service collection.
         var services = new ServiceCollection();
+
+        // Configure AWS services.
         services.AddDefaultAWSOptions(configuration.GetAWSOptions());
         services.AddAWSService<IAmazonDynamoDB>(configuration.GetAWSOptions("DynamoDb"));
+
+        // Register application options.
         services.AddOptions<ApplicationOptions>();
+
+        // Register the DynamoDB context.
         services.AddTransient<IDynamoDBContext, DynamoDBContext>();
+
+        // Register validators from the assembly containing the $command$Validator.
         services.AddValidatorsFromAssemblyContaining<$command$Validator>();
+
+        // Register MediatR and register services from the assembly containing $command$.
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof($command$).Assembly));
+
+        // Build and return the service provider.
         return services.BuildServiceProvider();
     }
 }
